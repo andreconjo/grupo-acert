84829faf447cc1fde0d75c0d95fe63b43e83b35c
andreconjo <andre.henrique@alu.ufc.br>
2019-09-05 19:43:38 -0300

    Swagger foi adicionado

--- src/main/java/br/com/grupoacert/conversion/api/ApiApplication.java
+++ src/main/java/br/com/grupoacert/conversion/api/ApiApplication.java
@@ -1,11 +1,23 @@
 package br.com.grupoacert.conversion.api;
 
+import com.google.common.base.Predicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
@@ -15,6 +27,26 @@ public class ApiApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(ApiApplication.class);
+	}
+
+	@Bean
+	public Docket demoApi() {
+		return new Docket(DocumentationType.SWAGGER_2)//<3>
+				.select()//<4>
+				.apis(RequestHandlerSelectors.any())//<5>
+				.paths(Predicates.not(PathSelectors.regex("/error.*")))//<6>, regex must be in double quotes.
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"Conversion API",
+				"Convert temperature.",
+				"API DEMO",
+				"",
+				new Contact("Andre Conjo", "https://www.linkedin.com/in/andre-conjo-27492299/", "andrehconjo@gmail.com"),
+				"License of API", "https://www.linkedin.com/in/andre-conjo-27492299/", Collections.emptyList());
 	}
 
 
